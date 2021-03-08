11294df564fbcb7f03963dc40607a77e2fb39197
Cynthyá <cynthya.belloni@gmail.com>
2021-03-08 15:00:46 +0000

    Distinct

---
+++ Distinct.py
@@ -0,0 +1,14 @@
+# Given an array A consisting of N integers, returns the number of distinct values in array A.
+# A = [2,1,1,2,3,1] -> should return 3 because we have 3 different numbers
+
+# Important
+# N is an integer within the range [0..100,000];
+# each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+def solution(A):
+    return len(set(A))   
+
+# Testing     
+print(solution([2,1,1,2,3,1]))
+
+# Detected time complexity: O(N*log(N)) or O(N)
